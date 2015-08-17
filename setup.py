1a257d81130e38c9b6c985a52b199f84c240c0f6
Matteo Bachetti <matteo.bachetti@irap.omp.eu>
2015-08-17 09:18:19 +0000

    Add six among the dependencies

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 # Do not change the variable name.  It's parsed by doc/conf.py script.
 version = '0.1.2'
 
-requires = ['Sphinx >= 1.2']
+requires = ['Sphinx >= 1.2', 'six']
 
 if sys.version_info < (2, 7):
     requires.append('argparse')
