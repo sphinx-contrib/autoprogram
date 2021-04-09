77de4f55851649bf677be5d4b44d4cfc4378404d
Nick <nick.guenther@polymtl.ca>
2021-04-09 14:19:37 -0400

    Declaring depending on python3.7+
    Fixes #47

--- setup.py
+++ setup.py
@@ -35,10 +35,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -49,6 +45,7 @@ setup(
         'Topic :: Software Development :: Documentation',
         'Topic :: Utilities'
     ],
+    python_requires='>=3.7',
     platforms='any',
     packages=find_packages(),
     namespace_packages=['sphinxcontrib'],
